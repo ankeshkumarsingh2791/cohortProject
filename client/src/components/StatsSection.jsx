f8576b708baa99d51bdb342b9a3cb00b2c8842e0
Aaditya Rana <aadityarana460@gmail.com>
2025-07-25 23:45:39 +0530

    home page design fixed and added more content to about page

--- client/src/components/StatsSection.jsx
+++ client/src/components/StatsSection.jsx
@@ -3,10 +3,10 @@ import CountUpCard from './utils/CountUpCard'
 
 const StatsSection = () => {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-3 gap-4 md:gap-6 p-4 md:p-8 bg-[#9746FF] text-white">
-      <CountUpCard label="Followers" target={10000} />
-      <CountUpCard label="Total Projects" target={1000} />
-      <CountUpCard label="Happy Clients" target={1000} />
+    <section className="w-full grid grid-cols-1 sm:grid-cols-3 gap-4 md:gap-6 p-4 md:p-8 bg-[#9746FF] text-white">
+      <CountUpCard label="Creators Collaborated With" target={10000} />
+      <CountUpCard label="Creaters Exclusively Managed" target={300} />
+      <CountUpCard label="Bankable Campaigns Executed" target={850} />
     </section>
   )
 }
--- client/src/components/utils/TypeWriter.jsx
+++ client/src/components/utils/TypeWriter.jsx
@@ -27,7 +27,7 @@ const TypeWriter = ({ text, speed = 100, delay = 0 }) => {
       initial={{ opacity: 0}}
       animate={{ opacity: 1}}
       transition={{ duration: 0.5 }}
-      className="w-full max-w-[90%] sm:max-w-[700px] md:max-w-[900px] lg:max-w-[1000px] p-4 mx-auto text-2xl sm:text-4xl md:text-6xl lg:text-7xl font-bold text-center bg-gradient-to-r from-white via-slate-100 to-[#6404DF] bg-clip-text text-transparent leading-tight sm:leading-snug md:leading-[5rem]"
+      className="w-full max-w-[90%] sm:max-w-[700px] md:max-w-[900px] lg:max-w-[1000px] p-4 mx-auto text-6xl sm:text-7xl md:text-8xl lg:text-9xl font-bold text-center bg-gradient-to-r from-white via-slate-100 to-[#6404DF] bg-clip-text text-transparent leading-[5rem] sm:leading-[6rem] md:leading-[7rem] lg:leading-[8rem]"
     >
       {displayedText}
     </motion.h2>
